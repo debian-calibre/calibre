48c908a26bbe375d5c1b646508a946e9d89cf69a
YOKOTA Hiroshi <yokota.hgml@gmail.com>
2019-12-08 12:29:39 +0900

    Add achiever options to reproducible build

--- debian/get-orig-source.sh
+++ debian/get-orig-source.sh
@@ -22,7 +22,7 @@ tar -Jxf ${c_file}
 
 rm -f ${c_dir}/src/odf/thumbnail.py
 rm -r ${c_dir}/resources/mathjax
-tar --use-compress-program="xz -9" -cf ../../../calibre_${V}+dfsg.orig.tar.xz ${c_dir}
+tar --use-compress-program="xz -9" --sort=name --owner=0 --group=0 --numeric-owner -cf ../../../calibre_${V}+dfsg.orig.tar.xz ${c_dir}
 
 cd ../..
 rm -r debian/orig
