72bfef57d2eac09562a44213c99be08433c84270
YOKOTA Hiroshi <yokota.hgml@gmail.com>
2020-06-19 19:30:55 +0900

    New upstream version 4.99.4+dfsg+really4.19.0

--- src/calibre/customize/profiles.py
+++ src/calibre/customize/profiles.py
@@ -452,7 +452,7 @@ class iPad3Output(iPadOutput):
 
 
 class TabletOutput(iPadOutput):
-    name = 'Tablet'
+    name = _('Tablet')
     short_name = 'tablet'
     description = _('Intended for generic tablet devices, does no resizing of images')
 
@@ -541,7 +541,7 @@ class SonyReaderT3Output(SonyReaderOutput):
 
 class GenericEink(SonyReaderOutput):
 
-    name = 'Generic e-ink'
+    name = _('Generic e-ink')
     short_name = 'generic_eink'
     description = _('Suitable for use with any e-ink device')
     epub_periodical_format = None
@@ -549,7 +549,7 @@ class GenericEink(SonyReaderOutput):
 
 class GenericEinkLarge(GenericEink):
 
-    name = 'Generic e-ink large'
+    name = _('Generic e-ink large')
     short_name = 'generic_eink_large'
     description = _('Suitable for use with any large screen e-ink device')
 
@@ -559,7 +559,7 @@ class GenericEinkLarge(GenericEink):
 
 class GenericEinkHD(GenericEink):
 
-    name = 'Generic e-ink HD'
+    name = _('Generic e-ink HD')
     short_name = 'generic_eink_hd'
     description = _('Suitable for use with any modern high resolution e-ink device')
 
