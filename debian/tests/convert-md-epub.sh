5c1d1c2e1c9c21de1464602b7700acb11be11e2a
YOKOTA Hiroshi <yokota.hgml@gmail.com>
2021-03-29 21:00:26 +0900

    Add more functionality test

--- debian/tests/convert-md-epub.sh
+++ debian/tests/convert-md-epub.sh
@@ -5,8 +5,40 @@ set -eu
 # Qt burfs some warnings to stderr when in Gnome-wayland session
 unset XDG_SESSION_TYPE
 
-output=${AUTOPKGTEST_ARTIFACTS}/convert-md-epub.epub
+################
+# ebook-convert
+################
+echo "ebook-convert"
 
-ebook-convert debian/tests/data/markdown.md ${output}
+mdown=debian/tests/data/markdown.md
+epub=${AUTOPKGTEST_ARTIFACTS}/convert-md-epub.epub
 
-epubcheck ${output}
+ebook-convert ${mdown} ${epub}
+
+#############
+# ebook-meta
+#############
+echo "ebook-meta"
+
+title_mdown=$(cat ${mdown}      | grep '^title' | cut -d : -f 2 | sed -e 's/^ \+//')
+title_epub=$(ebook-meta ${epub} | grep '^Title' | cut -d : -f 2 | sed -e 's/^ \+//')
+
+echo "Markdown title : ${title_mdown}"
+echo "ePub     title : ${title_epub}"
+
+if [ "${title_mdown}" != "${title_epub}" ] ; then
+    exit 1
+fi
+
+epubcheck ${epub}
+
+###############
+# ebook-polish
+###############
+echo "ebook-polish"
+
+epub_polished=${AUTOPKGTEST_ARTIFACTS}/convert-md-epub_pol.epub
+
+ebook-polish --add-soft-hyphens --compress-images --remove-unused-css ${epub} ${epub_polished}
+
+epubcheck ${epub_polished}
