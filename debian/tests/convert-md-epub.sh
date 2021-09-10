c8bdc1478c08a2a88554dc5f3b6b7f9f66d7b5d6
YOKOTA Hiroshi <yokota.hgml@gmail.com>
2021-09-10 14:53:36 +0900

    Cosmetic fix for output text

--- debian/tests/convert-md-epub.sh
+++ debian/tests/convert-md-epub.sh
@@ -34,8 +34,8 @@ echo "Test: ebook-meta"
 title_mdown=$(cat ${mdown}      | grep '^title' | cut -d : -f 2 | sed -e 's/^ \+//')
 title_epub=$(ebook-meta ${epub} | grep '^Title' | cut -d : -f 2 | sed -e 's/^ \+//')
 
-echo "Markdown title : ${title_mdown}"
-echo "ePub     title : ${title_epub}"
+echo "Markdown title : \"${title_mdown}\""
+echo "ePub     title : \"${title_epub}\""
 
 if [ "${title_mdown}" != "${title_epub}" ] ; then
     exit 1
