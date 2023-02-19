6ece68b4ae9fa4dbd48dd08b7b651c295924adbc
YOKOTA Hiroshi <yokota.hgml@gmail.com>
2023-02-19 10:40:15 +0900

    Add recent User-Agent downloader for deterministic build

---
+++ debian/download-recent_uas.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -eux
+
+output_dir=./debian/resources-src/recent_uas/
+
+curl --output ${output_dir}/common_user_agents.bz2 --cacert resources/calibre-ebook-root-CA.crt https://code.calibre-ebook.com/ua-popularity
+
+curl --output ${output_dir}/firefox_versions.html https://www.mozilla.org/en-US/firefox/releases/
+
+curl --output ${output_dir}/chrome_versions.html https://en.wikipedia.org/wiki/Google_Chrome_version_history
