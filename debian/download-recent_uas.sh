e5881e0c79b6f8877e73d79f21f3f4b186cbfa89
YOKOTA Hiroshi <yokota.hgml@gmail.com>
2023-02-20 02:22:30 +0900

    Prefer text files instead of binary file

--- debian/download-recent_uas.sh
+++ debian/download-recent_uas.sh
@@ -4,7 +4,7 @@ set -eux
 
 output_dir=./debian/resources-src/recent_uas/
 
-curl --output ${output_dir}/common_user_agents.bz2 --cacert resources/calibre-ebook-root-CA.crt https://code.calibre-ebook.com/ua-popularity
+curl --output - --cacert resources/calibre-ebook-root-CA.crt https://code.calibre-ebook.com/ua-popularity | bunzip2 > ${output_dir}/common_user_agents
 
 curl --output ${output_dir}/firefox_versions.html https://www.mozilla.org/en-US/firefox/releases/
 
